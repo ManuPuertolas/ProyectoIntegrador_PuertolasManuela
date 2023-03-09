7cfcda7a752b85633334bca116e14591daa5bff3
ManuPuertolas <manupuertolas6@gmail.com>
2023-03-09 11:29:55 -0300

    armado header y banner

--- ProyectoIntegrador_Puertolas/src/app/app.module.ts
+++ ProyectoIntegrador_Puertolas/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedSocialComponent,
+    BannerComponent,
+    AcercaDeMiComponent
   ],
   imports: [
     BrowserModule,
